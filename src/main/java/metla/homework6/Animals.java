4bac7fda8f6eef9e737f5170aa3c563172734dee
Метлицкий Александр <telegram @beard_one>
2022-05-16 20:36:42 +0300

    Метлицкий(metla boroda)

--- src/main/java/metla/homework6/Animals.java
+++ src/main/java/metla/homework6/Animals.java
@@ -3,40 +3,11 @@ package metla.homework6;
 import java.util.Scanner;
 
 public class Animals {
-    int height;
-    double weight;
-    String color;
-    public Animals(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("введите рост");
-        height = sc.nextInt();
+    int run;
+    int swim;
 
-        System.out.println("введите вес");
-        weight = sc.nextDouble();
-
-        System.out.println("введите цвет");
-        color = sc.next();
-    }
-    public class cat extends Animals {
-        String name;
-        String breed;
-        private int run = 200;
-        private int swim =0;
-
-        public cat (){
-            Scanner sc = new Scanner(System.in);
-
-            System.out.println("имя кота");
-            name= sc.next();
-
-            System.out.println("порода");
-            breed = sc.next();
-
-        }
-
-        public void checkRun(int )
-        }
-
-
-    }
+    public Animals() {}
 }
+
+
+
