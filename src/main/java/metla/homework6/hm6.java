30d38fb713f0c8f1b8dbc56e1d12f0566763fe72
Метлицкий Александр <telegram @beard_one>
2022-05-16 20:45:43 +0300

    Метлицкий(metla boroda)

--- src/main/java/metla/homework6/hm6.java
+++ src/main/java/metla/homework6/hm6.java
@@ -4,14 +4,17 @@ public class hm6 {
     public static void main(String[] args) {
         Cat murzik = new Cat(200,0);
         Dog bobby = new Dog(500,10);
+
         murzik.checkRun(199);
         murzik.checkRun(201);
+
         murzik.checkSwim(0);
         murzik.checkSwim(1);
+
         bobby.checkRun(499);
         bobby.checkRun(501);
+
         bobby.checkSwim(9);
         bobby.checkSwim(11);
-
     }
 }
